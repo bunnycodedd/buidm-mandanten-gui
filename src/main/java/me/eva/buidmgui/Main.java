4bd16acf341a536db7642059b387f7ea27e34641
Eva T <thoeva796@gmail.com>
2025-07-06 11:17:24 +0200

    connection screen layout demo

--- src/main/java/me/eva/buidmgui/Main.java
+++ src/main/java/me/eva/buidmgui/Main.java
@@ -12,26 +12,16 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             try {
                 launch();
-            } catch (UnsupportedLookAndFeelException e) {
-                throw new RuntimeException(e);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (InstantiationException e) {
-                throw new RuntimeException(e);
-            } catch (IllegalAccessException e) {
+            } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
+                     IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
     private static void launch() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        MainPage frame = new MainPage();
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        MainPage frame = new MainPage();
     }
 
 }
